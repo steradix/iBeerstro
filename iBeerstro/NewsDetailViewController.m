3768e472fc34e0104093cea57fb04cb10bb12ecc
Stefano Radice <Stefano@MacBook-Pro-di-Stefano.local>
2014-12-15 13:52:40 +0100

    first commit

--- iBeerstro/NewsDetailViewController.m
+++ iBeerstro/NewsDetailViewController.m
@@ -16,6 +16,7 @@
 
 @end
 
+
 @implementation NewsDetailViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
---
+++ iBeerstroTests/iBeerstroTests.m
@@ -0,0 +1,34 @@
+//
+//  iBeerstroTests.m
+//  iBeerstroTests
+//
+//  Created by Stefano Radice on 19/05/14.
+//  Copyright (c) 2014 Stefano Radice. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iBeerstroTests : XCTestCase
+
+@end
+
+@implementation iBeerstroTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
